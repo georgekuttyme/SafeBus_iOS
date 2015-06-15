d7108d33ecf8b3374508341eb5ef85cbad6294ee
veena-rainconcert <veena@rainconcert.in>
2015-06-15 15:53:04 +0530

    Initial Commit

---
+++ SafeBusTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SafeBusTracker
+//
+//  Created by veena on 15/06/15.
+//  Copyright (c) 2015 RainConcert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SafeBusTracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SafeBusTracker
+//
+//  Created by veena on 15/06/15.
+//  Copyright (c) 2015 RainConcert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
